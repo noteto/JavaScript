61c39afedecfcab09baafbd0403f84df67f54174
noteto <nevtod@gmail.com>
2013-04-02 00:14:23 +0300

    Update MaxSum.js

--- MaxSum.js
+++ MaxSum.js
@@ -1,23 +1,22 @@
-<script>
 function Solve(args)
     {
          
-        var N = parseInt(args[0]);
+        var N = parseInt(args[0]); 
  
-        var myArray = [];
+        var myArray = []; //new array where I push all elements from args[]
         var maxSum = 0;
         var i = 0;
-        var maxSums =[];
-        var firstMax=parseInt(args[1]);
+        var maxSums =[]; //new array where I push all possible sums of elements from myArray
+        var firstMax=parseInt(args[1]); //initialize max sum 
         var maximum = 0;
         var answer = 0;
  
         for(i=0;i<N;i++)
         {
-            myArray.push(parseInt(args[i+1]));
+            myArray.push(parseInt(args[i+1])); //taking every element from args[] and convert it to INT
         }
          
-        for(i=0;i<myArray.length;i++)
+        for(i=0;i<myArray.length;i++)  //find maximal value in the array
         {
             if(myArray[i]>firstMax)
             {
@@ -25,9 +24,9 @@ function Solve(args)
             }
         }
  
-        maxSum = myArray[0];
+        maxSum = myArray[0];  //now I'm gonna sum all the possibles sequences of elements and find which one is max
  
-        while(myArray.length>=1)
+        while(myArray.length>=1)  // count elements and reccord all possible sums of sequences in new array maxSums[]
         {
             for(i=1;i<myArray.length;i++)
             {
@@ -35,13 +34,13 @@ function Solve(args)
                 maxSums.push(maxSum);
             }
  
-            myArray.shift();
-            maxSum = myArray[0];
+            myArray.shift();    // remove the first element until there are no more than 1 elements left in myArray
+            maxSum = myArray[0]; // make maxSum = new first element and count again
         }
  
         maximum = maxSums[0];
  
-        for(i=0;i<maxSums.length;i++)
+        for(i=0;i<maxSums.length;i++) //at last find the maximal number in the array with all possible sums
         {
             if(maxSums[i] > maximum)
             {
@@ -49,7 +48,7 @@ function Solve(args)
             }
         }
  
-        if(maximum>firstMax)
+        if(maximum>firstMax)  //last check for negative sequences - finding the biggest value
         {
             answer = maximum;
         }
@@ -60,4 +59,3 @@ function Solve(args)
  
         return answer;
     }
-</script>
